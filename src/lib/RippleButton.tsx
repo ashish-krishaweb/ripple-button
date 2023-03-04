f0177ed87607c465ba5784fb208d72aad84422a4
Ashish Prajapati <ashish.p@krishaweb.com>
2023-03-04 08:59:20 +0530

    Add publish.yml file

--- src/lib/RippleButton.tsx
+++ src/lib/RippleButton.tsx
@@ -2,40 +2,44 @@ import * as React from "react";
 import "./RippleButton.scss";
 
 
-if(typeof window != "undefined"){
-    document.body.addEventListener("click", (e) =>{
-        // @ts-ignore
-        if(e.target.nodeName == "BUTTON" && e.target!.classList!.contains("simple-btn")){
-            // @ts-ignore
-            const handleClick = TargetMap.get(e.target.dataset.btnId);
-            // @ts-ignore
-            // const handleClick = TargetMap.get(e.target);
-            if(handleClick){
-                const span = document.createElement("span")
-                const button = e.target
-                if(button){
-                    // @ts-ignore
-                    const {left, top} = button.getBoundingClientRect()
+try{
+  if(typeof window != "undefined"){
+      document.body.addEventListener("click", (e) =>{
+          // @ts-ignore
+          if(e.target.nodeName == "BUTTON" && e.target!.classList!.contains("simple-btn")){
+              // @ts-ignore
+              const handleClick = TargetMap.get(e.target.dataset.btnId);
+              // @ts-ignore
+              // const handleClick = TargetMap.get(e.target);
+              if(handleClick){
+                  const span = document.createElement("span")
+                  const button = e.target
+                  if(button){
+                      // @ts-ignore
+                      const {left, top} = button.getBoundingClientRect()
 
-                    // @ts-ignore
-                    button.appendChild(span)
-                    // @ts-ignore
-                    span.style.cssText = `--width: ${button.getBoundingClientRect().width}px;--height: ${button.getBoundingClientRect().width}px`;
-                    // @ts-ignore
-                    span.style.left = (e.pageX - left)+'px'
-                    // @ts-ignore
-                    span.style.top = (e.pageY - top)+'px'
-                    span.style.animation = "ripple 0.5s linear"
-                    span.addEventListener("animationend", () =>{
-                        span.remove()
-                    })
-                }
+                      // @ts-ignore
+                      button.appendChild(span)
+                      // @ts-ignore
+                      span.style.cssText = `--width: ${button.getBoundingClientRect().width}px;--height: ${button.getBoundingClientRect().width}px`;
+                      // @ts-ignore
+                      span.style.left = (e.pageX - left)+'px'
+                      // @ts-ignore
+                      span.style.top = (e.pageY - top)+'px'
+                      span.style.animation = "ripple 0.5s linear"
+                      span.addEventListener("animationend", () =>{
+                          span.remove()
+                      })
+                  }
 
-                // @ts-ignore
-                handleClick()
-            }
-        }
-    })
+                  // @ts-ignore
+                  handleClick()
+              }
+          }
+      })
+  }
+}catch(err){
+  
 }
 const TargetMap = new Map()
 
